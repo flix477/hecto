b7c8f01e590587ab479c818a03578b6be87ef650
Félix Léveillé <flxleveille@gmail.com>
2019-11-22 22:11:48 -0500

    wip

--- src/core/posts/folders.rs
+++ src/core/posts/folders.rs
@@ -6,6 +6,7 @@ use std::path::{Path, PathBuf};
 
 type Entry = FolderEntry<Post, Folder>;
 
+/// Filters a list of folder entries to only get the posts
 pub fn posts<'a, T>(entries: T) -> impl Iterator<Item = &'a Post>
 where
     T: Iterator<Item = &'a Entry>,
@@ -19,6 +20,7 @@ where
     })
 }
 
+/// Filters a list of folder entries to only get the posts as mutable references
 pub fn mut_posts<'a, T>(entries: T) -> impl Iterator<Item = &'a mut Post>
 where
     T: Iterator<Item = &'a mut Entry>,
@@ -32,6 +34,7 @@ where
     })
 }
 
+/// Recursively gets all the posts in a folder
 pub fn recursive_posts(folder: &Folder) -> Vec<&Post> {
     folder
         .entries
@@ -43,6 +46,7 @@ pub fn recursive_posts(folder: &Folder) -> Vec<&Post> {
         .collect()
 }
 
+/// Filters a list of folder entries to only get the folders
 pub fn folders<'a, T>(entries: T) -> impl Iterator<Item = &'a Folder>
 where
     T: Iterator<Item = &'a Entry>,
@@ -56,6 +60,7 @@ where
     })
 }
 
+/// Filters a list of folder entries to only get the folders as mutable references
 pub fn mut_folders<'a, T>(entries: T) -> impl Iterator<Item = &'a mut Folder>
 where
     T: Iterator<Item = &'a mut Entry>,
@@ -69,6 +74,7 @@ where
     })
 }
 
+/// Gets the folder entry at a given path
 pub fn element_at<'a>(
     folder: &'a Folder,
     path: &Path
@@ -77,32 +83,36 @@ pub fn element_at<'a>(
 
     path.components()
         .map(component_as_string)
-        .try_fold(folder.into(), |acc: FolderEntry<&'a Post, &'a Folder>, component| {
-            acc.folder()
-                .and_then(|folder|
-                    folder.entries.iter()
-                        .find(|entry| entry.name() == component)
-                )
-                .map(FolderEntry::as_ref)
-        })
+        .try_fold(folder.into(), element_at_string)
 }
 
+fn element_at_string<'a>(acc: FolderEntry<&'a Post, &'a Folder>, component: String)
+               -> Option<FolderEntry<&'a Post, &'a Folder>>
+{
+    acc.folder()
+        .and_then(|folder| folder.get_entry(&component))
+        .map(FolderEntry::as_ref)
+}
+
+/// Gets the folder entry at a given path as a mutable reference
 pub fn mut_element_at<'a>(
     folder: &'a mut Folder,
     path: &Path
 ) -> Option<FolderEntry<&'a mut Post, &'a mut Folder>> {
     path.components()
         .map(component_as_string)
-        .try_fold(folder.into(), |acc: FolderEntry<&'a mut Post, &'a mut Folder>, component| {
-            acc.folder()
-                .and_then(|folder|
-                    folder.entries.iter_mut()
-                        .find(|entry| entry.name() == component)
-                )
-                .map(FolderEntry::as_mut_ref)
-        })
+        .try_fold(folder.into(), mut_element_at_string)
 }
 
+fn mut_element_at_string<'a>(acc: FolderEntry<&'a mut Post, &'a mut Folder>, component: String)
+                      -> Option<FolderEntry<&'a mut Post, &'a mut Folder>>
+{
+    acc.folder()
+        .and_then(|folder| folder.get_mut_entry(&component))
+        .map(FolderEntry::as_mut_ref)
+}
+
+/// Gets a list of all the paths to all the posts
 pub fn list(folder: &Folder, base_path: &Path) -> Vec<PathBuf> {
     folder
         .entries
--- src/server/mod.rs
+++ src/server/mod.rs
@@ -5,6 +5,7 @@ use hyper::service::service_fn_ok;
 use hyper::rt::Future;
 use std::path::Path;
 use crate::renderer::ToHtml;
+use std::error::Error;
 
 pub fn run(app: Arc<Mutex<Hecto>>) {
     let address = app.lock().unwrap().config.address.clone();
@@ -25,21 +26,32 @@ pub fn fetch_page(app: Arc<Mutex<Hecto>>) -> impl Fn(Request<Body>) -> Response<
         let app = app.lock().unwrap();
 
         let element = app.element_at_path(Path::new(path));
+
         if let Some(entry) = element {
             entry.to_html(&app.renderer)
-                .map(|html| Response::new(Body::from(html)))
-                .unwrap_or_else(|error| {
-                    eprintln!("{}", error);
-                    Response::builder()
-                        .status(StatusCode::INTERNAL_SERVER_ERROR)
-                        .body(Body::empty())
-                        .unwrap()
-                })
+                .map(on_render_success)
+                .unwrap_or_else(on_render_error)
         } else {
-            Response::builder()
-                .status(StatusCode::NOT_FOUND)
-                .body(Body::empty())
-                .unwrap()
+            not_found()
         }
     }
+}
+
+fn on_render_success(html: String) -> Response<Body> {
+    Response::new(Body::from(html))
+}
+
+fn on_render_error(error: Box<dyn Error>) -> Response<Body> {
+    eprintln!("Rendering error: {}", error);
+    Response::builder()
+        .status(StatusCode::INTERNAL_SERVER_ERROR)
+        .body(Body::empty())
+        .unwrap()
+}
+
+fn not_found() -> Response<Body> {
+    Response::builder()
+        .status(StatusCode::NOT_FOUND)
+        .body(Body::empty())
+        .unwrap()
 }
